13932de3ae00af30f5e0530817459f2fb125196c
hahas507 <aq12361@gmail.com>
2021-07-12 17:19:39 +0900

    Dynamic Enemy
    Dynamic Enemies(Ghost0~3) added.
    Moves by using NavMesh.

---
+++ John Lemon/Assets/Scripts/WayPointPatrol.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WayPointPatrol : MonoBehaviour
+{
+    [SerializeField]
+    private NavMeshAgent navMeshAgent;
+
+    [SerializeField]
+    private Transform[] waypoints;
+
+    private int m_CurrentWaypointIndex;
+
+    private void Start()
+    {
+        navMeshAgent.SetDestination(waypoints[0].position);
+    }
+
+    private void Update()
+    {
+        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
+
+            navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
+        }
+    }
+}
